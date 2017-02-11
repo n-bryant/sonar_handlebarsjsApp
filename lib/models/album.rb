1c9d2b4aef491918a495d3151364ff65c20d1672
Adam Reed <kusa.xisa@gmail.com>
2017-02-11 17:39:38 -0500

    Added image column

--- lib/models/album.rb
+++ lib/models/album.rb
@@ -6,7 +6,6 @@ class Album < ActiveRecord::Base
   belongs_to :band
   has_many :album_genres
   has_many :genres, through: :album_genres
-  has_one :image
   belongs_to :label
   has_many :songs
 end
