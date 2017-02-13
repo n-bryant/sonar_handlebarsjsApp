334c9b12659f21b70edfebed6680589669dbe959
Adam Reed <kusa.xisa@gmail.com>
2017-02-12 21:18:35 -0500

    Added search, edit, add, and delete

--- lib/resources/genres.rb
+++ lib/resources/genres.rb
@@ -1,7 +1,9 @@
 require_relative '../models/genre'
 
 get '/genre' do
-  genres = Genre.all
+  search = params.fetch('name', '')
+  genres = Genre.where("lower(name) LIKE (?)", "%#{search.downcase}%")
+
   halt [404, 'No genres found'.to_json] if genres.empty?
 
   genres.to_json
@@ -15,4 +17,37 @@ get '/genre/:id/bands' do |id|
   halt [400, 'No bands found for that genre.'.to_json] if bands.empty?
 
   bands.to_json
+end
+
+post '/genre' do
+  request.body.rewind
+  request_payload = JSON.parse request.body.read
+
+  genre = Genre.new(name: request_payload['name'])
+
+  halt [400, 'Name cannot be blank.'.to_json] unless genre.valid?
+
+  genre.save
+  [201, genre.to_json]
+end
+
+put '/genre/:id' do |id|
+  request.body.rewind
+  request_payload = JSON.parse request.body.read
+
+  genre = Genre.find_by_id(id)
+  halt [400, 'Genre not found.'.to_json] if genre.nil?
+
+  updated = genre.update(request_payload['name'])
+  halt [400, 'Name cannot be blank.'.to_json] unless updated
+
+  [200, genre.to_json]
+end
+
+delete '/genre/:id' do |id|
+  genre = Genre.find_by_id(id)
+  halt [400, 'No genre with that id, unable to delete.'.to_json] if genre.nil?
+
+  genre.destroy
+  genre.to_json
 end
