9261b806fd4b695a48278897e47e315d8b8bf3e9
Adam Reed <kusa.xisa@gmail.com>
2017-02-12 23:33:50 -0500

    Fixed chained dependencies

--- lib/models/song.rb
+++ lib/models/song.rb
@@ -4,6 +4,8 @@ class Song < ActiveRecord::Base
   validates :name, :release_date, presence: true
   belongs_to :album
   belongs_to :band
+  has_many :album_songs, dependent: :destroy
+  has_many :albums, through: :album_songs
   has_many :song_genres, dependent: :destroy
   has_many :genres, through: :song_genres
 
